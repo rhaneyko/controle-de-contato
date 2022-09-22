4bf946e711f017866229fd321a18e7ad3b49dec9
rhaneyko <rhaneykohonrio@gmail.com>
2022-09-22 14:59:03 -0400

    C# App

---
+++ controle-de-contato/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace controle_de_contato.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

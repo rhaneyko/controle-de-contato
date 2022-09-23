28cef156c8d59489df45acaae0af3b85f7d14eba
rhaneyko <rhaneykohonrio@gmail.com>
2022-09-23 14:38:28 -0400

    C# App

--- controle-de-contato/Controllers/ContatoController.cs
+++ controle-de-contato/Controllers/ContatoController.cs
@@ -23,4 +23,20 @@ namespace cadastro_de_contato.Controllers
         }
 
     }
+
+     public IActionResult Apagar(int id)
+        {
+            try
+            {
+                bool apagado = _contatoRepositorio.Apagar(id);
+
+                if(apagado) TempData["MensagemSucesso"] = "Contato apagado com sucesso!"; else TempData["MensagemErro"] = "Ops, não conseguimos cadastrar seu contato, tente novamante!";
+                return RedirectToAction("Index");
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Ops, não conseguimos apagar seu contato, tente novamante, detalhe do erro: {erro.Message}";
+                return RedirectToAction("Index");
+            }
+        }
 }
